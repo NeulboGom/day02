a0691322a3a307c392d45584da80873c2dca5a4d
NeulboGom <jinhee930409@inha.edu>
2023-01-13 14:01:24 +0900

    day02 ASCII and Arithmetic modified

--- day02_03.py
+++ day02_03.py
@@ -1,7 +1,8 @@
 '''
 # 진법
 #Binary number=0b10011010
-#Hex number=0x9A
+#Hex number=0x9A A=1010.....? 9A=1001....? ==> 16진수는 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F 으로
+                                               이루어져 있음 십진수 기준 A=10,B=11,C=12,D=13,E=14,F=15
 #Octal number=0o232
 print(number)
 
