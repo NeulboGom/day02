f0c7106b2b37a2d58f90e05ed3beb6618e8e1b04
NeulboGom <jinhee930409@inha.edu>
2023-01-13 13:35:54 +0900

    day02 Numbers

---
+++ day02_03.py
@@ -0,0 +1,18 @@
+'''
+#Chapter 3: Number
+print(divmod(9,5))
+print(type(divmod(9,5)))  #몫과 나머지를 동시에 얻는 방법
+#print(type(1,2))
+test=1,2 #packing
+print(type(test))
+print(test[1])
+a,b=test #unpacking
+print(a)
+print(b)
+'''
+
+
+# 진법
+print(0b1010)
+
+
