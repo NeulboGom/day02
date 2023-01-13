30f0a925caa22cdb60582bd23d8cd2ddd5896b86
NeulboGom <jinhee930409@inha.edu>
2023-01-13 12:57:21 +0900

    day02 Dictionary

---
+++ day02_01.py
@@ -0,0 +1,11 @@
+'''
+harry_porter={"Hermione":"Griffindor","Malfoy":"Sliderin","Malfoy":"Griffindor"}
+print(harry_porter)
+print(type(harry_porter))
+'''
+
+#dictionary의 키값은 중복 불가
+
+harry_porter={"Hermione":"Griffindor","Malfoy":"Sliderin","Ron":"Griffindor"}
+print(harry_porter)
+print(type(harry_porter))
